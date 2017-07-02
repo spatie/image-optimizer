cd8fd6fe3caeebf2feeb38ed8fd4cb298abe8876
freek <freek@spatie.be>
2017-07-02 20:36:13 +0200

    add skeleton

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Spatie\Skeleton\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
