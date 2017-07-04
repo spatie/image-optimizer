bc2541cd0f7b5e4a555cf224795b88f7e4158e89
freek <freek@spatie.be>
2017-07-04 10:17:23 +0200

    add image tests

---
+++ tests/ImageTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Spatie\ImageOptimizer\Test;
+
+use InvalidArgumentException;
+use Spatie\ImageOptimizer\Image;
+
+class ImageTest extends TestCase
+{
+    /** @test */
+    public function it_will_throw_an_exception_when_given_a_non_existing_file()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Image('non existing file');
+    }
+
+    /** @test */
+    public function it_can_get_type_mime_type()
+    {
+        $image = new Image($this->getTestFilePath('test.jpg'));
+
+        $this->assertEquals('image/jpeg', $image->mime());
+    }
+
+    /** @test */
+    public function it_can_get_the_path()
+    {
+        $path = $this->getTestFilePath('test.jpg');
+
+        $image = new Image($path);
+
+        $this->assertEquals($path, $image->path());
+    }
+}
