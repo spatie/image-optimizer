be422492a556ece422c6588deec8d534df8ff804
Robbert <robbertluit@hotmail.com>
2018-06-06 23:48:14 +0200

    alphabetize use paths

--- tests/OptimizerChainFactoryTest.php
+++ tests/OptimizerChainFactoryTest.php
@@ -2,12 +2,12 @@
 
 namespace Spatie\ImageOptimizer\Test;
 
-use Spatie\ImageOptimizer\Optimizers\Svgo;
-use Spatie\ImageOptimizer\Optimizers\Optipng;
-use Spatie\ImageOptimizer\Optimizers\Gifsicle;
-use Spatie\ImageOptimizer\Optimizers\Pngquant;
-use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
 use Spatie\ImageOptimizer\OptimizerChainFactory;
+use Spatie\ImageOptimizer\Optimizers\Gifsicle;
+use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
+use Spatie\ImageOptimizer\Optimizers\Optipng;
+use Spatie\ImageOptimizer\Optimizers\Pngquant;
+use Spatie\ImageOptimizer\Optimizers\Svgo;
 
 class OptimizerChainFactoryTest extends TestCase
 {
