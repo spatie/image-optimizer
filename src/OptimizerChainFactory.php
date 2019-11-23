0d714783e2a0c8d4f964eb5bdc7bc840be637f71
Freek Van der Herten <freek@spatie.be>
2019-11-23 17:54:54 +0000

    Apply fixes from StyleCI

--- src/OptimizerChainFactory.php
+++ src/OptimizerChainFactory.php
@@ -2,12 +2,12 @@
 
 namespace Spatie\ImageOptimizer;
 
-use Spatie\ImageOptimizer\Optimizers\Svgo;
 use Spatie\ImageOptimizer\Optimizers\Cwebp;
-use Spatie\ImageOptimizer\Optimizers\Optipng;
 use Spatie\ImageOptimizer\Optimizers\Gifsicle;
-use Spatie\ImageOptimizer\Optimizers\Pngquant;
 use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
+use Spatie\ImageOptimizer\Optimizers\Optipng;
+use Spatie\ImageOptimizer\Optimizers\Pngquant;
+use Spatie\ImageOptimizer\Optimizers\Svgo;
 
 class OptimizerChainFactory
 {
