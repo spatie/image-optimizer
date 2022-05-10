804c11f04fb60f1154c18f6b2ceaa644160e8aa5
Mansoor Khan <8mansoorkhan@gmail.com>
2022-05-10 09:58:50 +0500

    Import Stringable in ArrayLogger.php

--- tests/ArrayLogger.php
+++ tests/ArrayLogger.php
@@ -3,52 +3,53 @@
 namespace Spatie\ImageOptimizer\Test;
 
 use Psr\Log\LoggerInterface;
+use Stringable;
 
 class ArrayLogger implements LoggerInterface
 {
     protected $logLines = [];
 
-    public function emergency(string|\Stringable $message, array $context = []): void
+    public function emergency(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "emergency: {$message}";
     }
 
-    public function alert(string|\Stringable $message, array $context = []): void
+    public function alert(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "alert: {$message}";
     }
 
-    public function critical(string|\Stringable $message, array $context = []): void
+    public function critical(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "critical: {$message}";
     }
 
-    public function error(string|\Stringable $message, array $context = []): void
+    public function error(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "error: {$message}";
     }
 
-    public function warning(string|\Stringable $message, array $context = []): void
+    public function warning(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "warning: {$message}";
     }
 
-    public function notice(string|\Stringable $message, array $context = []): void
+    public function notice(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "notice: {$message}";
     }
 
-    public function info(string|\Stringable $message, array $context = []): void
+    public function info(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "info: {$message}";
     }
 
-    public function debug(string|\Stringable $message, array $context = []): void
+    public function debug(string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "debug: {$message}";
     }
 
-    public function log($level, string|\Stringable $message, array $context = []): void
+    public function log($level, string|Stringable $message, array $context = []): void
     {
         $this->logLines[] = "log: {$message}";
     }
