b11bf8d412424a884c5ac33fa947ddb8d1f50b30
erikn69 <erikn69@users.noreply.github.com>
2024-10-31 16:58:22 +0000

    Fix styling

--- src/OptimizerChain.php
+++ src/OptimizerChain.php
@@ -66,7 +66,7 @@ class OptimizerChain
     {
         if ($pathToOutput) {
             $check = copy($pathToImage, $pathToOutput);
-            if($check == false) {
+            if ($check == false) {
                 throw new InvalidArgumentException("Cannot copy file");
             }
             $pathToImage = $pathToOutput;
